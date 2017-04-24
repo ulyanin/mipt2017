e48d0ccc156cd8960afba87e3f5a984f772cd0a4
Ulyanin Dmitry <ulyanin1997@gmail.com>
2017-04-24 15:20:02 +0300

    Lab0 and Lab1

--- lab0/Lab0.hs
+++ lab0/Lab0.hs
@@ -7,18 +7,24 @@ import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Char8 as C
 import Network (withSocketsDo)
 
-(email, name) = ("", encodeUtf8 "") -- адрес почты и фамилия с инициалами
+(email, name) = ("ulyanin1997@gmail.com", encodeUtf8 "Ульянин Д.А.") -- адрес почты и фамилия с инициалами
 
 pascal :: Int -> Int -> Int
-pascal c r = 1 -- а тут решение
+pascal k n
+        | k == 0    = 1
+        | n == k    = 1
+        | k > n     = 0
+        | otherwise = (pascal (k - 1) (n - 1)) + (pascal k (n - 1))
+
 
 printIt :: Int -> C.ByteString
 printIt n = C.pack $ show $ [pascal y x | x <- [0..n], y <- [0..x]]
 
+
 main :: IO()
-main = 
+main =
   withSocketsDo $ do
   initReq <- parseUrl "http://91.239.142.110:13666/lab0"
   let req = urlEncodedBody [("email", email), ("name", name), ("content", printIt 20)] $ initReq { method = "POST" }
   response <- withManager $ httpLbs req
-L.putStr $ responseBody response
+  L.putStr $ responseBody response
--- lab1/Lab1.hs
+++ lab1/Lab1.hs
@@ -1,63 +1,140 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Lab1 (fTailor, tailor, tailorA, fSolve, iter, newton, dichotomy) where
+module Lab1 (main, fTailor, tailor, tailorA, fSolve, iter, newton, dichotomy) where
 
-import System.IO
-import System.Environment
-import System.Directory
-import Control.Monad
-import Network.HTTP.Conduit
+import           Control.Monad
 import qualified Data.ByteString.Lazy.Char8 as L
-import Data.Text.Encoding
-import qualified Data.Text as T
-import Network (withSocketsDo)
+import           Data.List
+import qualified Data.Text                  as T
+import           Data.Text.Encoding
+import           Network                    (withSocketsDo)
+import           Network.HTTP.Conduit
+import           System.Directory
+import           System.Environment
+import           System.IO
 
 -- почтовый адрес
-email = ""
+email = "ulyanin1997@gmail.com"
 -- общий тип для возвращаемых вашими функциями значений, где первая часть кортежа это само значение функции, вторая - кол-во операций
-type Result = (Float, Integer)
+type Result = (Double, Integer)
 
-fTailor x = x -- функция, которую раскладываем
+
+-- variant 18
+fTailor :: Floating f => f -> f
+fTailor x = (1.0 + x * x) / 2.0 * atan x - x / 2.0 -- функция, которую раскладываем
+
+delta :: Double
 delta = 1e-10
-(n, a, b) = (20, -1, 1) -- интервал
+(n, a, b) = (20, 0.1, 0.6) -- интервал
 
-tailor x = (x, 0)
+tailor :: Double -> Result
+tailor x = (last listTerms, fromIntegral $ length listTerms)
+    where
+        realRes = fTailor x
+        listTerms = Data.List.unfoldr (\(k, sumRes) ->
+                                    if abs (realRes - sumRes) < delta then
+                                        Nothing
+                                    else
+                                        Just (sumRes, (k + 1, sumRes + nthTerm k))) (1, 0.0)
+        nthTerm :: Integer -> Double
+        nthTerm k = fromIntegral ((-1) ^ (k + 1)) * (x ^ fromIntegral (2 * k + 1)) / fromIntegral (4 * k ^ 2 - 1)
 
-tailorA :: Float -> Result
-tailorA x = (x, 0)
+tailorA :: Double -> Result
+tailorA x = (last listTerms, fromIntegral $ length listTerms)
+    where
+        realRes = fTailor x
+        listTerms = Data.List.unfoldr (\(k, currentTerm, sumRes) ->
+                                    if abs (realRes - sumRes) < delta then
+                                        Nothing
+                                    else
+                                        Just (sumRes, (k + 1, nextTerm currentTerm (k + 1), sumRes + currentTerm))) (1, nthTerm 1, 0.0)
+        nthTerm :: Integer -> Double
+        nthTerm k = fromIntegral ((-1) ^ (k + 1)) * (x ^ fromIntegral (2 * k + 1)) / fromIntegral (4 * k ^ 2 - 1)
 
-printTailor = mapM_ putStrLn $ 
-	map 
-		(\ x -> 
-			let ((firstRes, firstCou), (secondRes, secondCou)) = (tailor x, tailorA x) 
-			in show x ++ "\t" ++ show firstRes ++ "\t" ++ show firstCou ++ "\t" ++ show secondRes ++ "\t" ++ show secondCou ++ "\t" ++ show (fTailor x)) 
+        nextTerm :: Double -> Integer -> Double
+        nextTerm fPrev k = fPrev * (-1) * x ^ 2 * fromInteger (4 * (k - 1) ^ 2 - 1) / fromIntegral (4 * k ^ 2 - 1)
+
+printTailor = mapM_ putStrLn $
+	map
+		(\ x ->
+			let ((firstRes, firstCou), (secondRes, secondCou)) = (tailor x, tailorA x)
+			in
+                show x ++ "\t" ++ show firstRes ++ "\t" ++ show firstCou ++ "\t" ++ 
+                show secondRes ++ "\t" ++ show secondCou ++ "\t" ++ show (fTailor x))
 		[a, a + (b - a) / n .. b]
+
 
 -- *** Вторая часть
 
-fSolve = \x -> x -- функция, решение которой ищем
+fun18 :: Double -> Double
+fun18 x = x + sqrt x + x ** (1.0 / 3.0) - 2.5
 
-iter :: (Float -> Float) -> Float -> Float -> Result
-iter f a b = (42, 0)
+-- d/dx fun18
+fun18' :: Double -> Double
+fun18' x = 1.0 + 1.0 / 2 / sqrt x + (1.0 / 3.0) / (x ** (2.0 / 3.0))
 
-newton :: (Float -> Float) -> Float -> Float -> Result
-newton f a b = (42, 0)
+fun19 :: Double -> Double
+fun19 x = x - 1.0 / (3 + sin (3.6 * x))
 
-dichotomy =    
-	--для функций с аккумулятором удобно ставить его в начало
-	let dichotomyA i f a b = (42, 0)
-	in dichotomyA 0 -- чтобы воспользоваться каррированием
+fun19' :: Double -> Double
+fun19' x = 1 + (3.6 * cos (3.6 * x)) / (sin (3.6 * x) + 3) ^ 2
+
+fun20 :: Double -> Double
+fun20 x = 0.1 * x ^ 2 - x * log x
+
+fun20' :: Double -> Double
+fun20' x = 0.2 * x  - log x - 1
+
+functions :: [(Double -> Double, Double -> Double, Double, Double)]
+functions = [(fun18, fun18', 0.4, 1), (fun19, fun19', 0, 0.85), (fun20, fun20', 1, 2)]
+
+-- fSolve = \x -> x -- функция, решение которой ищем
+fSolve = fun19
+
+iter :: (Double -> Double) -> (Double -> Double) -> Double -> Double -> Result
+iter f f' a b = iter' ((a + b) / 2) 0
+    where
+        k = 1 / f' b
+        iter' x n
+                | abs (k * f x) < delta = (x, n)
+                | otherwise             = iter' (x - k * f x) (n + 1)
+
+newton :: (Double -> Double) ->(Double -> Double) -> Double -> Double -> Result
+newton f f' a b = newton' ((b + a) / 2) 0
+    where
+        newton' x n
+                | abs curDelta < delta = (x, n)
+                | otherwise             = newton' (x - curDelta) (n + 1)
+                where curDelta = f x / f' x
+
+dichotomy :: (Double -> Double) ->(Double -> Double) -> Double -> Double -> Result
+dichotomy f f' = dichotomy' 0
+    where
+        dichotomy' n a b
+                        | abs (b - a) < delta   = (m, n)
+                        | f b * f m < 0         = dichotomy' (n + 1) m b
+                        | otherwise             = dichotomy' (n + 1) a m
+                        where m = (a + b) / 2
 
 printSolve =
-	mapM_ putStrLn $ map (\f -> show $ f fSolve a b) [iter, newton, dichotomy]
+	mapM_ putStrLn
+            [
+                "\n[" ++ show a ++ "; "++ show b ++ "]\n" ++
+                    unwords
+                        (map (\f -> show $ f fSolve fSolve' a b) [iter, newton, dichotomy])
+                | (fSolve, fSolve', a, b) <- functions
+            ]
 
+main :: IO()
 main = withSocketsDo $ do
-  dir <- getCurrentDirectory
-  initReq <- parseUrl "http://91.239.142.110:13666/lab1"
-  handle <- openFile (dir ++ "/Lab1.hs") ReadMode
-  hSetEncoding handle utf8_bom
-  content <- hGetContents handle
-  let req = urlEncodedBody [("email", email), ("content", encodeUtf8 $ T.pack content) ] $ initReq { method = "POST" }
-  response <- withManager $ httpLbs req
-  hClose handle
-  L.putStrLn $ responseBody response
+    printTailor
+    dir <- getCurrentDirectory
+    -- initReq <- parseUrl "http://91.239.142.110:13666/lab1"
+    initReq <- parseUrl "http://192.168.80.59:13666"
+    handle <- openFile (dir ++ "/Lab1.hs") ReadMode
+    hSetEncoding handle utf8_bom
+    content <- hGetContents handle
+    let req = urlEncodedBody [("email", email), ("content", encodeUtf8 $ T.pack content) ] $ initReq { method = "POST" }
+    response <- withManager $ httpLbs req
+    hClose handle
+    L.putStrLn $ responseBody response
